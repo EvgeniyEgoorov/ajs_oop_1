96e2a20a2ab5642d07d3247eb21464458c7b7e87
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-26 19:57:30 +0800

    feat: added base class

---
+++ src/js/character.js
@@ -0,0 +1,19 @@
+export default class Character {
+    constructor(name, type, attack, defence) {
+        if (name.length < 2 || name.length > 10) {
+            throw new Error('Invalid name length');
+        }
+        const types = [
+            'Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie',
+        ];
+        if (!types.includes(type)) {
+            throw new Error('Invalid character type');
+        }
+        this.name = name;
+        this.type = type;
+        this.health = 100;
+        this.level = 1;
+        this.attack = attack;
+        this.defence = defence;
+    }
+}
