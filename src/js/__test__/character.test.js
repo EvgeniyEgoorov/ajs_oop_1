675218de807ccfad04fb19497585abc3c4c64a7a
Egorov Evgeniy <egorovzhenya@gmail.com>
2023-01-29 10:33:53 +0800

    fix: updated tests structure

--- src/js/__test__/character.test.js
+++ src/js/__test__/character.test.js
@@ -26,60 +26,72 @@ test('Invalid character type', () => {
 
 test('Bowman', () => {
     const bowman = new Bowman('Bowy');
-    expect(bowman.name).toEqual('Bowy');
-    expect(bowman.type).toEqual('Bowman');
-    expect(bowman.health).toEqual(100);
-    expect(bowman.level).toEqual(1);
-    expect(bowman.attack).toEqual(25);
-    expect(bowman.defence).toEqual(25);
+    expect(bowman).toEqual({
+        "name": "Bowy", 
+        "type": "Bowman",
+        "health": 100, 
+        "level": 1,
+        "attack": 25, 
+        "defence": 25, 
+    })
 });
 
 test('Daemon', () => {
     const daemon = new Daemon('Daemy');
-    expect(daemon.name).toEqual('Daemy');
-    expect(daemon.type).toEqual('Daemon');
-    expect(daemon.health).toEqual(100);
-    expect(daemon.level).toEqual(1);
-    expect(daemon.attack).toEqual(10);
-    expect(daemon.defence).toEqual(40);
+    expect(daemon).toEqual({
+        "name": "Daemy", 
+        "type": "Daemon",
+        "health": 100, 
+        "level": 1,
+        "attack": 10, 
+        "defence": 40, 
+    })
 });
 
 test('Magician', () => {
     const magician = new Magician('Magy');
-    expect(magician.name).toEqual('Magy');
-    expect(magician.type).toEqual('Magician');
-    expect(magician.health).toEqual(100);
-    expect(magician.level).toEqual(1);
-    expect(magician.attack).toEqual(10);
-    expect(magician.defence).toEqual(40);
+    expect(magician).toEqual({
+        "name": "Magy", 
+        "type": "Magician",
+        "health": 100, 
+        "level": 1,
+        "attack": 10, 
+        "defence": 40, 
+    })
 });
 
 test('Swordsman', () => {
     const swordsman = new Swordsman('Swordy');
-    expect(swordsman.name).toEqual('Swordy');
-    expect(swordsman.type).toEqual('Swordsman');
-    expect(swordsman.health).toEqual(100);
-    expect(swordsman.level).toEqual(1);
-    expect(swordsman.attack).toEqual(40);
-    expect(swordsman.defence).toEqual(10);
+    expect(swordsman).toEqual({
+        "name": "Swordy", 
+        "type": "Swordsman",
+        "health": 100, 
+        "level": 1,
+        "attack": 40, 
+        "defence": 10, 
+    })
 });
 
 test('Undead', () => {
     const undead = new Undead('Undy');
-    expect(undead.name).toEqual('Undy');
-    expect(undead.type).toEqual('Undead');
-    expect(undead.health).toEqual(100);
-    expect(undead.level).toEqual(1);
-    expect(undead.attack).toEqual(25);
-    expect(undead.defence).toEqual(25);
+    expect(undead).toEqual({
+        "name": "Undy", 
+        "type": "Undead",
+        "health": 100, 
+        "level": 1,
+        "attack": 25, 
+        "defence": 25, 
+    })
 });
 
 test('Zombie', () => {
     const zombie = new Zombie('Zomy');
-    expect(zombie.name).toEqual('Zomy');
-    expect(zombie.type).toEqual('Zombie');
-    expect(zombie.health).toEqual(100);
-    expect(zombie.level).toEqual(1);
-    expect(zombie.attack).toEqual(40);
-    expect(zombie.defence).toEqual(10);
+    expect(zombie).toEqual({
+        "name": "Zomy", 
+        "type": "Zombie",
+        "health": 100, 
+        "level": 1,
+        "attack": 40, 
+        "defence": 10, 
+    })
 });
